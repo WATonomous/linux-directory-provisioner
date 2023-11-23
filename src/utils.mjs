1e891fe5786b6c60c45349ee9368dbad382e9f5b
Ben Zhang <ben.zhang@watonomous.ca>
2023-11-23 01:50:45 +0000

    Refactor schema into its own file

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,65 +1,8 @@
 import Ajv from 'ajv';
 import { readFile } from 'node:fs/promises';
+import { configSchema } from './schema.mjs';
 
 const ajv = new Ajv({ allErrors: true, strict: true, useDefaults: true });
-
-const userSchema = {
-  type: 'object',
-  properties: {
-    username: { type: 'string' },
-    password: { type: 'string' },
-    update_password: { enum: ['always', 'on_create'] },
-    uid: { type: 'number' },
-    primary_group: { type: 'string' },
-    additional_groups: {
-      type: 'array',
-      items: { type: 'string' },
-      uniqueItems: true,
-      default: [],
-    },
-    shell: { type: 'string', default: '/bin/bash' },
-    ssh_authorized_keys: { type: 'array', items: { type: 'string' }, default: [] },
-  },
-  required: ['username', 'password', 'update_password', 'uid', 'primary_group'],
-  additionalProperties: false,
-};
-
-const groupSchema = {
-  type: 'object',
-  properties: {
-    groupname: { type: 'string' },
-    gid: { type: 'number' },
-  },
-  required: ['groupname', 'gid'],
-  additionalProperties: false,
-};
-
-const configSchema = {
-  type: 'object',
-  properties: {
-    users: { type: 'array', items: userSchema },
-    groups: { type: 'array', items: groupSchema },
-    managed_uid_range: {
-      type: 'array',
-      description:
-        'Range of UIDs that are managed by this script. Users with UIDs in this range will be deleted if they are not in the config.',
-      items: { type: 'number' },
-      minItems: 2,
-      maxItems: 2,
-    },
-    managed_gid_range: {
-      type: 'array',
-      description:
-        'Range of GIDs that are managed by this script. Groups with GIDs in this range will be deleted if they are not in the config.',
-      items: { type: 'number' },
-      minItems: 2,
-      maxItems: 2,
-    },
-    user_ssh_key_base_dir: { type: 'string', description: 'Base directory for user SSH keys.', default: '/home' },
-  },
-  required: ['users', 'groups'],
-  additionalProperties: false,
-};
 
 export const validateConfig = ajv.compile(configSchema);
 
